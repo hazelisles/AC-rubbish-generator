756368169bacb28d284b7d21a1db1e237a48165a
hazelisles <zhccj19@gmail.com>
2021-05-01 00:20:00 +0800

    feat: modify generate rubbish function

--- generate_rubbish.js
+++ generate_rubbish.js
@@ -4,11 +4,11 @@ function generateRubbish(choice) {
   const end = '吧！'
   const target = ['工程師', '設計師', '創業家']
   const task = {
-  engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
-  designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
-  entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+  engineer: ['加個按鈕', '加個新功能', '切個版', '改一點 code', '設計個軟體', '架個網站'],
+  designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計', '想個設計 idea', '畫個透視圖'],
+  entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢', '隨便說個創業 idea']
   }
-  const phrase = ['很簡單','很容易','很快','很正常']
+  const phrase = ['很簡單', '很容易', '很快', '很正常', '不難']
 
   const profession = Object.keys(choice)[0]
 
@@ -20,18 +20,15 @@ function generateRubbish(choice) {
     case 'engineer':
       name = target[0]
       tk = task.engineer[randomNumber(task.engineer)]
-      rubbish = start.concat(name, '，', tk, '，', ph, end)
-      return rubbish
+      return buildUpRubbish(start, name, tk, ph, end)
     case 'designer':
       name = target[1]
       tk = task.designer[randomNumber(task.designer)]
-      rubbish = start.concat(name, '，', tk, '，', ph, end)
-      return rubbish
+      return buildUpRubbish(start, name, tk, ph, end)
     case 'entrepreneur':
       name = target[2]
       tk = task.entrepreneur[randomNumber(task.entrepreneur)]
-      rubbish = start.concat(name, '，', tk, '，', ph, end)
-      return rubbish
+      return buildUpRubbish(start, name, tk, ph, end)
     default:
       return '請選擇一個說幹話的對象！'
   }
@@ -39,6 +36,10 @@ function generateRubbish(choice) {
 
 function randomNumber(array) {
   return Math.floor(Math.random() * array.length)
+}
+
+function buildUpRubbish(start, name, tk, ph, end) {
+  return start.concat(name, '，', tk, '，', ph, end)
 }
 
 module.exports = generateRubbish
